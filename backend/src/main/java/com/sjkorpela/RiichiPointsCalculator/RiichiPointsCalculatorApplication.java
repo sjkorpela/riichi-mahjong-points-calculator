b44133af789193fb58a85534956ca61b3be2381d
Santeri Korpela <sjkorpela03@gmail.com>
2026-01-29 19:26:00 +0200

    Clean up and document Main class

--- backend/src/main/java/com/sjkorpela/RiichiPointsCalculator/RiichiPointsCalculatorApplication.java
+++ backend/src/main/java/com/sjkorpela/RiichiPointsCalculator/RiichiPointsCalculatorApplication.java
@@ -1,12 +1,19 @@
 package com.sjkorpela.RiichiPointsCalculator;
 
-import com.sjkorpela.RiichiPointsCalculator.Services.JsonService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main class of Riichi Points Calculator.
+ */
 @SpringBootApplication
 public class RiichiPointsCalculatorApplication {
 
+	/**
+	 * Spring Boot default app runner function.
+	 *
+	 * @param args command line arguments, not used
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(RiichiPointsCalculatorApplication.class, args);
 	}
